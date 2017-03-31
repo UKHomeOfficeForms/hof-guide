d2d25517cfcd60cfb4d3ba6d942e8e6bb8fd6f86
Leonard Martin <leonard.martin@gmail.com>
2017-03-31 15:28:39 +0100

    Add auto deploy gh-pages

--- config.rb
+++ config.rb
@@ -59,6 +59,20 @@ configure :build do
   # activate :minify_javascript
 end
 
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = 'gh-pages' # or master
+
+  committer_app = "#{Middleman::Deploy::PACKAGE} v#{Middleman::Deploy::VERSION}"
+  commit_message = "Deployed using #{committer_app}"
+
+  if ENV["TRAVIS_BUILD_NUMBER"] then
+    commit_message += " (Travis Build \##{ENV["TRAVIS_BUILD_NUMBER"]})"
+  end
+
+  deploy.commit_message = commit_message
+end
+
 ###
 # Tech Docs-specific configuration
 ###
