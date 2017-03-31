9f7cb2cb4b1eeed4ad5dd8201ef880396f0a0cc0
Leonard Martin <leonard.martin@gmail.com>
2017-03-31 15:53:31 +0100

    Guesswork

--- config.rb
+++ config.rb
@@ -60,17 +60,12 @@ configure :build do
 end
 
 activate :deploy do |deploy|
-  deploy.method = :git
-  deploy.branch = 'gh-pages' # or master
-
-  committer_app = "#{Middleman::Deploy::PACKAGE} v#{Middleman::Deploy::VERSION}"
-  commit_message = "Deployed using #{committer_app}"
-
+  deploy.deploy_method = :git
   if ENV["TRAVIS_BUILD_NUMBER"] then
-    commit_message += " (Travis Build \##{ENV["TRAVIS_BUILD_NUMBER"]})"
+    committer_app = "#{Middleman::Deploy::PACKAGE} v#{Middleman::Deploy::VERSION}"
+    commit_message = "Deployed using #{committer_app}"
+    deploy.commit_message = "#{commit_message} (Travis Build \##{ENV["TRAVIS_BUILD_NUMBER"]})"
   end
-
-  deploy.commit_message = commit_message
 end
 
 ###
