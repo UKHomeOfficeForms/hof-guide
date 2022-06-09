8f6381879883dc27997b5a5a52832a0c4707634d
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2022-06-09 18:29:09 +0100

    Add wbkit scrollbar functionality

--- src/components/layout.js
+++ src/components/layout.js
@@ -49,7 +49,7 @@ const Layout = ({ children }) => (
             </div>
           </main>
         </div>
-        <Footer/>
+                <Footer/>
         <button id="scroll-btn" className="sidebar-btns" title="Scroll to top"></button>
         <Helmet>
             <script src={withPrefix('js/govuk.js')} type="text/javascript" />
