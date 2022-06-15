ee4e6e2cce5ec1c5c2883e376dfa1aa5bd9856d8
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2022-06-15 01:41:07 +0100

    Add an index of pages relevant to the topic directory under breadcrumbs

--- src/templates/directory-index.js
+++ src/templates/directory-index.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-import { graphql } from 'gatsby';
+import _ from 'lodash';
+import { graphql, useStaticQuery, Link } from 'gatsby';
 
 import Layout from '../components/layout';
 
 const Template = ({ pageContext }) => {
+  function titleCase(str) {
+    str = str.toLowerCase();
+    str = str.split(' ');
+
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+  }
+
   const crumbs = pageContext.breadcrumb.crumbs;
   const adjustedTitle = pageContext.title.replace(/_/g, ' ');
+  const {
+    pages: { nodes },
+  } = useStaticQuery(graphql`
+    {
+      pages: allSitePage {
+        nodes {
+          path
+        }
+      }
+    }
+  `);
+  const sitePages = nodes.map(node => node.path);
+  let relevantUris = sitePages.filter(page => page.startsWith(pageContext.slug));
+  relevantUris = relevantUris.filter(page => page !== pageContext.slug);
+  relevantUris = relevantUris.map(path => {
+    return { path, title: titleCase(path.split('/').reverse()[0].replace(/[-_]/g, ' ').replace(/^([0-9]+)/g, '-')) };
+  });
+  relevantUris = _.sortBy(relevantUris, 'title');
 
   return (
     <Layout crumbs={crumbs} title={adjustedTitle}>
       <Helmet title={adjustedTitle} />
       <div>
         <h1>{adjustedTitle}</h1>
-        <div dangerouslySetInnerHTML={{ __html: '<></>' }} />
+        <p>The following pages refer to this topic:</p>
+        <ul class="directory-index">
+          {relevantUris.map(obj => {
+              return (
+                <li><Link to={obj.path}>{obj.title}</Link></li>
+             )
+          })}
+        </ul>
       </div>
     </Layout>
   );
